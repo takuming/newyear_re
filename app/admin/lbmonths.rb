b7d73fb41e5a021835953787d18aa4872b15293d
カイクン <takumi.kai.skywalker@gmail.com>
2019-12-13 09:48:01 +0900

    lbmonthのモデルを作った

---
+++ app/admin/lbmonths.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Lbmonth do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :jan, :feb, :mar, :apr, :may, :jun, :jul, :aug, :sep, :oct, :nov, :dec
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:jan, :feb, :mar, :apr, :may, :jun, :jul, :aug, :sep, :oct, :nov, :dec]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
