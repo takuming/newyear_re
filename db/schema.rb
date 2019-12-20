8182b2ecf72b8a889eb63fa656b5e7606f902d08
カイクン <takumi.kai.skywalker@gmail.com>
2019-12-20 20:46:58 +0900

    goalモデルにUserを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_090935) do
+ActiveRecord::Schema.define(version: 2019_12_19_232045) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2019_12_15_090935) do
     t.text "problem"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "lbcuriosities", force: :cascade do |t|
