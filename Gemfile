0dfa078c1053aa1f06ab22bd94a4f824313606cc
カイクン <takumi.kai.skywalker@gmail.com>
2019-12-31 20:20:03 +0900

    ogp設定

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.5.5'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~>1.3.6', group: :development 
+gem 'sqlite3', '~>1.3.6'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -15,6 +15,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
+
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
@@ -70,3 +71,5 @@ gem 'activeadmin'
 gem 'devise'
 
 gem 'pg', group: :production 
+
+gem 'meta-tags'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title: 'NEWYEAR',
+      reverse: true,
+      description: 'ディスクリプション',
+      keywords: '',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+      ],
+      og: {
+        site_name: 'NEWYEAR',
+        title: 'NEWYEAR 今年のじぶんと旅に出る | １年の振り返りから、来年やってみたいことがつくれるセルフコーチングサービス',
+        description: '今年のじぶんと旅に出る | １年の振り返りから、来年やってみたいことがつくれるセルフコーチングサービス', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@takumii_kai',
+      }
+      fb: {
+        app_id: ''
+      }
+    }
+  end
 end
